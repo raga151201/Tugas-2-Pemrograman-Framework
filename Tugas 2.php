2d3386a5795e7281c5e358638d54f905c15c9357
raga151201 <107837444+raga151201@users.noreply.github.com>
2022-10-04 21:06:51 +0700

    Create Tugas 2.php

---
+++ Tugas 2.php
@@ -0,0 +1,16 @@
+<?php
+//class mobil
+Class Mobil{
+    public $nama;
+    public $merk;
+function getInfo(){
+echo "Nama mobil: ".$this->nama."<br/>";
+echo "Merk: ".$this->merk."<br/>";
+}
+}
+//bagian main
+$ferari=new Mobil();
+$ferari->nama="xxx";
+$ferari->merk="aaa";
+$ferari->getInfo();
+?>
